4d4fe3190dcfb9a3da96ec854af81aed15fc365f
gauravnair2000 <gauravnair2000@gmail.com>
2016-01-23 20:56:01 +0530

    Plot3_update
    Plot 3 updated to include legend

--- plot3.R
+++ plot3.R
@@ -21,4 +21,7 @@ with(sub,plot(Time,Sub_metering_1,type="n",xlab="",ylab="Energy sub metering"))
 lines(sub$Time,sub$Sub_metering_1)
 lines(sub$Time,sub$Sub_metering_3,type="l",col="blue")
 lines(sub$Time,sub$Sub_metering_2,type="l",col="red")
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=1,col=c("black","red","blue"),bty="n",cex=.75)
+
+
 dev.off()
